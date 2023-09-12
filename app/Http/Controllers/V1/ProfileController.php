ba30cb1ff703007d057b35c1ed459534b1f13b60
onyedikachukwu <onyedikachukwu62@gmail.com>
2023-09-12 20:25:08 +0100

    Added some info

--- app/Http/Controllers/V1/ProfileController.php
+++ app/Http/Controllers/V1/ProfileController.php
@@ -24,22 +24,50 @@ class ProfileController extends Controller
 
         if($user->type === 'talent'){
 
+            if (empty($user->skill_title) || empty($user->top_skills) || empty($user->highest_education) || empty($user->year_obtained) || empty($user->work_history) || empty($user->certificate_earned) || empty($user->availability)) {
+                $onboarding = false;
+            } else {
+                $onboarding = true;
+            }
+
+            if (empty($user->compensation) || empty($user->portfolio_title) || empty($user->portfolio_description) || empty($user->image)) {
+                $port = false;
+            } else {
+                $port = true;
+            }
+
             $details = new TalentResource($user);
 
             return [
                 "status" => 'true',
-                "data" => $details
+                "data" => $details,
+                'work_details' => $onboarding,
+                'portofolio' => $port
             ];
 
         }
 
         if($user->type === 'business'){
 
+            if (empty($user->business_name) || empty($user->location) || empty($user->industry) || empty($user->about_business) || empty($user->website) || empty($user->business_service) || empty($user->business_email)) {
+                $onboarding = false;
+            } else {
+                $onboarding = true;
+            }
+
+            if (empty($user->company_logo) || empty($user->company_type) || empty($user->social_media)) {
+                $port = false;
+            } else {
+                $port = true;
+            }
+
             $details = new BusinessResource($user);
 
             return [
                 "status" => 'true',
-                "data" => $details
+                "data" => $details,
+                'business_details' => $onboarding,
+                'portofolio' => $port
             ];
 
         }
