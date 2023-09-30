ea2fa98b7ed2851b602a0fa92933ae1659353242
onyedikachukwu <onyedikachukwu62@gmail.com>
2023-09-30 08:18:02 +0100

    Email

--- resources/views/v1/mail/talentverify.blade.php
+++ resources/views/v1/mail/talentverify.blade.php
@@ -9,14 +9,6 @@
         background: #F9F9F9;
         padding: 50px 100px;
     }
-    h1{
-        color: #007582;
-        font-family: Satoshi;
-        font-size: 40px;
-        font-style: normal;
-        font-weight: 400;
-        line-height: 74.647px;
-    }
     .trial{
         color: #011B1F;
         font-family: Satoshi;
@@ -34,7 +26,7 @@
         line-height: 37.471px;
     }
     h3{
-        color: #011B1F;
+        color: #007582;
         font-family: Satoshi;
         font-size: 25px;
         font-style: normal;
@@ -51,11 +43,13 @@
     }
     button{
         border: 0;
-        border-radius: 20px;
+        border-radius: 35px;
         background: #43D0DF;
-        color: #222;
-        padding: 10px 20px;
+        color: #000;
+        padding: 15px 25px;
         margin: 10px 0;
+        font-weight: 500;
+        font-size: 13px;
     }
     button:hover{
         cursor: pointer;
@@ -68,33 +62,21 @@
         <div class="logo">
             <img src="https://myspurr.azurewebsites.net/logo/logo.svg" width="240px" height="52px" alt="">
         </div>
-        <h1>Welcome to My Spurr!</h1>
+        <H3>Verify your email address</H3>
 
         <p class="trial">
-            Your 30 day free trial has started.
-        </p>
-        <p>
-            MySpurr was created for creative talents, we're passionate about supporting talents like you by finding the perfect match for your skills and interests and providing the tools you need to succeed.
-        </p>
-        <h3>Ready to <span class="charge">super charge your creative career?</span></h3>
-        <p>
-            You can showcase your portfolio, connect directly with creative talents like yourself, create and send invoices, apply for jobs in the creative industry, receive payments for your services and access a growing list of global businesses always on the look out for creative talents.
+            Welcome to MySpurr! Click on the following link to verify your email address
         </p>
         <div>
-            <a href="{{ $otp }}"><button>Confirm my email</button></a>
+            <a href="{{ $otp }}"><button>VERIFY EMAIL ADDRESS</button></a>
         </div>
         <p>
-            Thanks again for trying out MySpurr!
-        </p>
-        <p>
-            We can’t wait to help you build a successful creative career. <br>
-            P.S. Lets connect on LinkedIn, I’d love you hear of your incredible journey with us
+            If you didn’t request this email or if you think something is wrong, feel free to
+            contact our support team at hello@myspurr.net. We’re available to help! <br><br>
+
+            <a href="">Instagram</a> <a href="">Facebook</a> <a href="">LinkedIn</a> <a href="">X (formerly twitter)</a>
         </p>
 
-        <p>
-            Tobi Akinyele <br>
-            CEO & Founder MySpurr
-        </p>
     </div>
 
 </body>
