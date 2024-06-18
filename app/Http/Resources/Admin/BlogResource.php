d260a237f066749955ab03c547e4d821b989a891
onyiimesi <onyedikachukwu62@gmail.com>
2024-06-18 10:30:12 +0100

    Blog feature update route

--- app/Http/Resources/Admin/BlogResource.php
+++ app/Http/Resources/Admin/BlogResource.php
@@ -21,10 +21,35 @@ class BlogResource extends JsonResource
             'slug' => (string)$this->slug,
             'description' => (string)$this->description,
             'content' => (string)$this->content,
-            'tags' => json_decode($this->tags),
+            'tags' => $this->getTagsAsArray(),
             'featured_photo' => (string)$this->featured_photo,
             'publish_date' => Carbon::parse($this->publish_date)->format('d M Y h:i A'),
-            'status' => (string)$this->status
+            'status' => (string)$this->status,
+            'created_at' => Carbon::parse($this->created_at)->format('d M Y h:i A'),
         ];
+    }
+
+    public function getTagsAsArray()
+    {
+        $tags = $this->tags;
+
+        // First decode
+        $decodedOnce = json_decode($tags, true);
+
+        // Check if the first decode result is an array
+        if (is_array($decodedOnce)) {
+            return $decodedOnce;
+        }
+
+        // If the first decode is not an array, try the second decode
+        $decodedTwice = json_decode($decodedOnce, true);
+
+        // Check for JSON errors and if the result is an array
+        if (json_last_error() === JSON_ERROR_NONE && is_array($decodedTwice)) {
+            return $decodedTwice;
+        }
+
+        // If decoding fails, return an empty array
+        return [];
     }
 }
