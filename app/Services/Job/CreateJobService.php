3f873ca6acf33cfefc62488fc6f442c151b794fb
onyiimesi <onyedikachukwu62@gmail.com>
2024-08-27 13:21:00 +0100

    Modification

--- app/Services/Job/CreateJobService.php
+++ app/Services/Job/CreateJobService.php
@@ -31,37 +31,34 @@ class CreateJobService {
     {
         $business = Business::where('email', $this->email)->first();
 
-        try {
-            $data = new TalentJob();
+        $data = new TalentJob();
 
-            $data->business_id = $business->id;
-            $data->job_title = $this->job['job_title'];
-            $data->slug = Str::slug($this->job['job_title']);
-            $data->country_id = $this->job['country_id'];
-            $data->state_id = $this->job['state_id'];
-            $data->job_type = $this->job['job_type'];
-            $data->description = $this->job['description'];
-            $data->responsibilities = $this->job['responsibilities'];
-            $data->required_skills = $this->job['required_skills'];
-            $data->benefits = $this->job['benefits'];
-            $data->salaray_type = $this->job['salaray_type'];
-            $data->salary_min = $this->job['salary_min'];
-            $data->salary_max = $this->job['salary_max'];
-            $data->currency = $this->job['currency'];
-            $data->skills = $this->job['skills'];
-            $data->experience = $this->job['experience'];
-            $data->qualification = $this->job['qualification'];
-            $data->is_highlighted = $this->highlight;
-            $data->status = 'active';
-            $data->save();
-
+        $data->business_id = $business->id;
+        $data->job_title = $this->job['job_title'];
+        $data->slug = Str::slug($this->job['job_title']);
+        $data->country_id = $this->job['country_id'];
+        $data->state_id = $this->job['state_id'];
+        $data->job_type = $this->job['job_type'];
+        $data->description = $this->job['description'];
+        $data->responsibilities = $this->job['responsibilities'];
+        $data->required_skills = $this->job['required_skills'];
+        $data->benefits = $this->job['benefits'];
+        $data->salaray_type = $this->job['salaray_type'];
+        $data->salary_min = $this->job['salary_min'];
+        $data->salary_max = $this->job['salary_max'];
+        $data->currency = $this->job['currency'];
+        $data->skills = $this->job['skills'];
+        $data->experience = $this->job['experience'];
+        $data->qualification = $this->job['qualification'];
+        $data->is_highlighted = $this->highlight;
+        $data->status = 'active';
+        $data->save();
+        
+        if (!empty($this->job['questions'])) {
             foreach ($this->job['questions'] as $questionData) {
                 $question = new Question($questionData);
                 $data->questions()->save($question);
             }
-
-        } catch (\Throwable $th) {
-            return $th->getMessage();
         }
 
         $business->talentjobtypes()->update(['is_active' => 0]);
