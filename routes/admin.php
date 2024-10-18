c6e72dde4a03b0f85747a93ad397817ccad3160f
onyiimesi <onyedikachukwu62@gmail.com>
2024-10-18 16:47:28 +0100

    Api

--- routes/admin.php
+++ routes/admin.php
@@ -29,6 +29,8 @@ Route::post('/file/upload', [OthersController::class, 'uploadFile']);
 Route::prefix('lookups')->controller(OthersController::class)
 ->group(function () {
     Route::get('/countries', 'getCountries');
+    Route::get('/state/{country_id}', 'getStates');
+    Route::get('/skills', 'getSkills');
 });
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
